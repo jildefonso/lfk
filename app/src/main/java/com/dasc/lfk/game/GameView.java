e80ff4421f231348c0a93f04976bb41a4aad8de2
Jesús Ildefonso <j.ildefonso@silifalcon.com>
2019-05-07 11:31:36 -0500

    Agrega el area de dibujo

--- app/src/main/java/com/dasc/lfk/game/GameView.java
+++ app/src/main/java/com/dasc/lfk/game/GameView.java
@@ -1,11 +1,16 @@
 package com.dasc.lfk.game;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import com.dasc.lfk.R;
 
 /**
  * Es la vista principal del juego, donde se dibujrá todo
@@ -19,6 +24,8 @@ public class GameView extends SurfaceView
      */
     private Paint paint;
 
+    private Bitmap pingu;
+
     /**
      * Crea la vista e inicializa el contexto
      * @param context Es el contexto de la aplicación android
@@ -26,6 +33,7 @@ public class GameView extends SurfaceView
     public GameView(Context context) {
         super(context);
         paint = new Paint();
+        pingu = BitmapFactory.decodeResource(getResources(), R.drawable.pingu);
         getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder surfaceHolder) {
@@ -52,10 +60,64 @@ public class GameView extends SurfaceView
      */
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        paint.setColor(Color.RED);
-        paint.setStrokeWidth(5);
-        canvas.drawLine(0, 0, 100, 800, paint);
+        drawBackground(canvas);
+        drawScenario(canvas);
+        drawPlayer(canvas);
+    }
+
+    private void drawBackground(Canvas canvas) {
+        paint.setColor(getResources()
+                .getColor(R.color.background, getContext().getTheme()));
+        canvas.drawRect(getLeft(), getTop(), getRight(), getBottom(), paint);
+    }
+
+    private int getOffsetX() {
+        int width = 1000 - 20;
+        int midX = (getRight() - getLeft()) / 2;
+        return midX - (width / 2);
+    }
+
+    private int getOffsetY() {
+        int height = 1000 - 20;
+        int midY = (getBottom() - getTop()) / 2;
+        return midY - (height / 2);
+
+    }
+
+    private void drawScenario(Canvas canvas) {
+        paint.setColor(getResources()
+                .getColor(R.color.scenario_dot, getContext().getTheme()));
+
+        int offsetX = getOffsetX();
+        int offsetY = getOffsetY();
+
+
+        for (int row = 0; row < 10; row++) {
+            for (int col = 0; col < 10; col++) {
+                canvas.drawOval(row * 100 + offsetX,
+                        col * 100 + offsetY,
+                        row * 100 + 80 + offsetX,
+                        col * 100 + 80 + offsetY, paint);
+            }
+        }
+    }
+
+    private void drawPlayer(Canvas canvas) {
+        int mx = 7;
+        int my = 4;
+
+        int offsetX = getOffsetX();
+        int offsetY = getOffsetY();
+
+        offsetX += mx * 100;
+        offsetY += my * 100;
+
+        Rect source = new Rect(0, 0, pingu.getWidth(), pingu.getHeight());
+        Rect target = new Rect(offsetX,
+                offsetY,
+                offsetX + 100 - 10,
+                offsetY + 100 - 10);
+        canvas.drawBitmap(pingu, source, target, paint);
     }
 
 }
